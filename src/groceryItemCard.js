a00ca1c6b1e2479b6c0594a94168e93eabfe9f83
lincoln anderson <lincolnanderson@me.com>
2022-05-17 22:34:23 +0000

    data object set up. testing for environment set up

--- src/groceryItemCard.js
+++ src/groceryItemCard.js
@@ -1,5 +1,8 @@
-export default function groceryItemCard(name, dateExpiring) {
-	<div className= "groceryCardItem">
-		<h2>{name}</h2>
-	</div>
+export default function GroceryItemCard({name, dateExpiring}) {
+	return (
+		<div className = "groceryItemCard">
+			<h3>{name}</h3>
+			<h2>{dateExpiring}</h2>
+		</div>
+	)
 }
