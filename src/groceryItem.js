98b84c6fe954956069d187c4333aa381864fff16
lincoln anderson <lincolnanderson@me.com>
2022-05-17 16:17:03 -0500

    started work on custom data structures

---
+++ src/groceryItem.js
@@ -0,0 +1,7 @@
+class GroceryItem {
+	constructor(name, datePurchased, dateExpiring) {
+		this.name = name
+		this.datePurchased = datePurchased
+		this.dateExpiring = dateExpiring
+	}
+}
---
+++ src/groceryItemCard.js
@@ -0,0 +1,5 @@
+export default function groceryItemCard(name, dateExpiring) {
+	<div className= "groceryCardItem">
+		<h2>{name}</h2>
+	</div>
+}
