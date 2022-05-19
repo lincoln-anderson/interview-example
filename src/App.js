b64fb5d60c4f4e98ccff093bf917dd5fab30b778
lincoln anderson <lincolnanderson@me.com>
2022-05-19 14:09:01 -0500

    its good

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import GroceryItem from './GroceryItem';
 import GroceryList from './GroceryList';
 import GroceryItemForm from './GroceryItemForm';
 import './App.css';
@@ -13,19 +12,11 @@ function App() {
   
   const [ groceryList, setGroceryList ] = useState(groceryitems);
   
-  const axios = require('axios');
-  
   const addItem = (userInput ) => {
     let copy = [...groceryList];
     copy = [...copy, {id: groceryList.length + 1, name: userInput.itemName, dateExpiring: userInput.dateExpiring, expired: false}]
     setGroceryList(copy)
   }
-  
-  
-  var myData = axios.get('http://localhost:3000/GroceryItems')
-  .then(function (response) {
-        return response.data;
-    })
   
   return (
     <div className="App">
--- src/groceryItem.js
+++ src/groceryItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GroceryItem = ({groceryItem}) => {
 	return (
 		<div id = {groceryItem.id} key = {groceryItem.id} className={groceryItem.expired ? "strike" : ""}>
-			{groceryItem.name}
+			{groceryItem.name} {groceryItem.dateExpiring}
 		</div>
 	);
 };
