f4ea4b47e182f0ef5c30324629898c03be3d5a3d
lincoln anderson <lincolnanderson@me.com>
2022-05-19 08:43:57 -0500

    stable version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import GroceryItemCard from './groceryItemCard';
 import './App.css';
 import React from "react";
-import groceryitems from "./groceryItems.json"
+import groceryitems from "./db.json"
+
 
 
 
 function App() {
+  
+  const axios = require('axios');
+  
+  var myData = axios.get('http://localhost:3000/GroceryItems')
+  .then(function (response) {
+        console.log(response.data)
+        return response.data;
+    })
+  console.log(myData)
+  
   return (
     <div className="App">
     
       <h1>Grocery List</h1>
         <div className="groceryitemCard">
-          <ul>
-            {groceryitems.map((item) => <GroceryItemCard key = {item.id} name={item.name} dateExpiring={item.dateExpiring} />)}
+          <ul className="groceryitemCard">
+            {groceryitems.GroceryItems.map((item) => <GroceryItemCard key = {item.id} name={item.name} dateExpiring={item.dateExpiring} />)}
           </ul>
         </div>
     </div>
