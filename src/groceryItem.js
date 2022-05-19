d73c59f6fc97954fc3959328694fa62d27f60f52
lincoln anderson <lincolnanderson@me.com>
2022-05-19 09:51:48 -0500

    expired works

--- src/groceryItem.js
+++ src/groceryItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GroceryItem = ({groceryItem}) => {
 	return (
-		<div id = {groceryItem.id} key = {groceryItem.id} className={groceryItem.expired ? "" : "strike"}>
+		<div id = {groceryItem.id} key = {groceryItem.id} className={groceryItem.expired ? "strike" : ""}>
 			{groceryItem.name}
 		</div>
 	);
